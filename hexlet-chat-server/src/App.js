cdb1015a98c73f711c2450618ae7c53dc67a3d6e
Vera Makhonya <vera.makhonya@gmail.com>
2024-07-15 12:17:16 +0300

    init

---
+++ hexlet-chat-server/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" style={{ fontSize: '24px' }}>
+        Инициализируем приложение для работы
+      </header>
+    </div>
+  );
+}
+
+export default App;
